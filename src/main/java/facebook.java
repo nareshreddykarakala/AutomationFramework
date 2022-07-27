d093f3d49d48c20b06af5ae3b45667e772ad3ac2
nareshreddykarakala <Nkaraka@gap.com>
2022-07-27 11:46:20 +0530

    facebook automation

--- src/main/java/facebook.java
+++ src/main/java/facebook.java
@@ -2,14 +2,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 public class facebook {
-    public void test(String toAddress) throws InterruptedException {
-        var toEmails = toAddress.split("[;,]");
-        System.out.println(toEmails[0]+"  "+ ""+toEmails[1]);
+    public void test() throws InterruptedException {
 
-        /*
+
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
@@ -20,10 +17,10 @@ public class facebook {
         driver.findElement(By.id("pass")).sendKeys("naresh@harika");
         Thread.sleep(5000);
         driver.findElement(By.ByName.name("login")).click();
-        Thread.sleep(7000);*/
+        Thread.sleep(7000);
     }
 
     public static void main(String[] args) throws InterruptedException {
-        new facebook().test("HQ-PriceItemPlanning-DG@gap.com;nareshkumar_reddy_karakala@gap.com");
+        new facebook().test();
     }
 }
